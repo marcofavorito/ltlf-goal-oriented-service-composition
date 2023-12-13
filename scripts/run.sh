9da4f67815832266e55f03e979813fbf0c07850b
Marco Favorito <marco.favorito@gmail.com>
2023-12-13 23:01:48 +0100

    update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,7 +6,7 @@ set -exuo pipefail
 ORIGINAL_DOMAIN=$1
 ORIGINAL_PROBLEM=$2
 ENCODING=$3
-ENCODING_ARGS=${4:-1}
+ENCODING_ARGS=${4:-}
 PLANNER=$5
 PLANNER_ARGS=${6:-}
 
@@ -26,8 +26,8 @@ fi
 if [[ -z "$PLANNER" ]]; then
   echo "Planner not set!"
   exit 1
-elif [[ "$PLANNER" != "fond-sat" && "$PLANNER" != "mynd" ]]; then
-  echo "Invalid planner: '$PLANNER'. Allowed values are 'fond-sat' and 'mynd'."
+elif [[ "$PLANNER" != "mynd" ]]; then
+  echo "Invalid planner: '$PLANNER'. Allowed values are {'mynd'}."
   exit 1
 fi
 
@@ -35,8 +35,8 @@ fi
 if [[ -z "$ENCODING" ]]; then
   echo "Encoding not set!"
   exit 1
-elif [[ "$ENCODING" != "TB" && "$ENCODING" != "ltlf2f" ]]; then
-  echo "Invalid encoding: '$ENCODING'. Allowed values are 'TB' and 'ltlf2f'."
+elif [[ "$ENCODING" != "TB" ]]; then
+  echo "Invalid encoding: '$ENCODING'. Allowed values are {'TB'}."
   exit 1
 fi
 
@@ -50,8 +50,8 @@ TB_encode() {
   if [[ -z "$_ACTION_MODE" ]]; then
     echo "Action mode not set!"
     exit 1
-  elif [[ "$_ACTION_MODE" != "1" && "$_ACTION_MODE" != "2" ]]; then
-    echo "Invalid action mode. Allowed values are 1 and 2."
+  elif [[ "$_ACTION_MODE" != "1" && "$_ACTION_MODE" != "2" && "$_ACTION_MODE" != "3" && "$_ACTION_MODE" != "4" ]]; then
+    echo "Invalid action mode. Allowed values are 1, 2, 3 and 4."
     exit 1
   fi
 
@@ -81,9 +81,9 @@ run_fondsat() {
 
 if [[ "$ENCODING" == "TB" ]]; then
   TB_encode "$ORIGINAL_DOMAIN" "$ORIGINAL_PROBLEM" "$ENCODING_ARGS"
-elif [[ "$ENCODING" == "ltlf2f" ]]; then
-  echo "Processing with ltlf2f encoding..."
-  exit 1;
+#elif [[ "$ENCODING" == "xxx" ]]; then
+#  echo "Processing with xxx encoding..."
+#  exit 1;
 else
   echo "Error: Invalid encoding option"
   exit 1
@@ -92,8 +92,8 @@ fi
 
 if [[ "$PLANNER" == "mynd" ]]; then
   run_mynd "${ORIGINAL_DOMAIN%.pddl}_compiled.pddl" "${ORIGINAL_PROBLEM%.pddl}_compiled.pddl" "$PLANNER_ARGS"
-elif [[ "$PLANNER" == "fond-sat" ]]; then
-  echo "Processing with fond-sat planner..."
+#elif [[ "$PLANNER" == "xxx" ]]; then
+#  echo "Processing with xxx planner..."
 else
   echo "Error: Invalid planner option"
   exit 1
