75b8b13066f9f8ab2c2cc6228f7c18aad77f7996
Marco Favorito <marco.favorito@gmail.com>
2023-12-23 19:01:23 +0100

    add exec permission to other scripts

--- scripts/compute-simplified-policies.sh
+++ scripts/compute-simplified-policies.sh
@@ -4,8 +4,4 @@ set -exuo pipefail
 
 OUTPUT_DIR=${1:-output}
 
-echo "Unzipping all large 'policy.dot' files..."
-for file in $(find output-final -name "policy.dot.zip"); do unzip $file -d $(dirname ${file}); done
-
-echo "Simplify all policies:"
 find output -name "policy.dot" | xargs dirname | xargs -I{} python3 ./scripts/simplify-policy.py --dot-file {}/policy.dot --out-file {}/simplified-policy.dot
